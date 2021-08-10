418a4bc3b42ba83afabb9894edee403e4c752eeb
Ignacio Loaiza <ignacio.loaiza@mail.utoronto.ca>
2021-08-10 16:59:27 -0500

    Added mp2 functions

---
+++ mp2.jl
@@ -0,0 +1,142 @@
+#CODE DRIVER FOR EXECUTION
+
+using Distributed
+@everywhere include("config.jl")
+
+#ARGS = [1=mol_name, 2=spin_orb, 3=opt_flavour, 4=frag_flavour, 5=u_flavour, 6=α_max]
+args_len = length(ARGS)
+const mol_name = ARGS[1]
+initial = false
+
+if SUPPRESSOR == false
+	if args_len >= 2
+		@everywhere const spin_orb = parse(Bool, remotecall_fetch(i->ARGS[i],1,2))
+	end
+	if args_len >= 3
+		@everywhere const opt_flavour = remotecall_fetch(i->ARGS[i],1,3)
+	end
+	if args_len >= 4
+		@everywhere const frag_flavour = remotecall_fetch(i->ARGS[i],1,4)
+	end
+	if args_len >= 5
+		@everywhere const u_flavour = remotecall_fetch(i->ARGS[i],1,5)
+	end
+	if args_len >= 6
+		@everywhere const α_max = parse(Int64, remotecall_fetch(i->ARGS[i],1,6))
+	end
+else
+	@everywhere using Suppressor
+	if args_len >= 2
+		@everywhere @suppress_err global spin_orb = parse(Bool, remotecall_fetch(i->ARGS[i],1,2))
+	end
+	if args_len >= 3
+		@everywhere @suppress_err global opt_flavour = remotecall_fetch(i->ARGS[i],1,3)
+	end
+	if args_len >= 4
+		@everywhere @suppress_err global frag_flavour = remotecall_fetch(i->ARGS[i],1,4)
+	end
+	if args_len >= 5
+		@everywhere @suppress_err global u_flavour = remotecall_fetch(i->ARGS[i],1,5)
+	end
+	if args_len >= 6
+		@everywhere @suppress_err global α_max = parse(Int64, remotecall_fetch(i->ARGS[i],1,6))
+	end
+end
+@everywhere include("include.jl")
+include("py_utils.jl")
+
+if initial != false
+	NAMES,INIT = loading(initial)
+	if NAMES[1] == "x0"
+		x0 = INIT[1]
+		K0 = INIT[2]
+	else
+		x0 = INIT[2]
+		K0 = INIT[1]
+	end
+	println("Loaded $NAMES")
+else
+	K0 = Int64[]
+	x0 = Float64[]
+end
+
+println("Starting MP2 calculations with:")
+@show mol_name
+@show opt_flavour
+@show frag_flavour
+@show u_flavour
+@show spin_orb
+@show wfs
+@show basis
+@show geometry 
+@show decomp_tol
+@show α_max
+@show verbose
+@show grad
+@show reps
+
+_, h_ferm = obtain_tbt(mol_name, basis=basis, ferm=true, spin_orb=spin_orb, geometry=geometry)
+tbt, Hccsd = obtain_ccsd(mol_name, basis=basis, spin_orb=spin_orb, geometry=geometry)
+
+if opt_flavour == "full-rank" || opt_flavour == "fr"
+	@time FRAGS = full_rank_driver(tbt, decomp_tol, reps = reps, α_max=α_max, grad=grad, verbose=verbose, x0=x0, K0=K0, spin_orb=spin_orb)
+elseif opt_flavour == "greedy" || opt_flavour == "g"
+	@time FRAGS = greedy_driver(tbt, decomp_tol, reps = reps, α_max=α_max, grad=grad, verbose=verbose, spin_orb=spin_orb)
+elseif opt_flavour == "og" || opt_flavour == "orthogonal-greedy"
+	println("Using λ=$λort for orthogonal greedy constrain value")
+	@time FRAGS = orthogonal_greedy_driver(tbt, decomp_tol, reps = reps, α_max=α_max, grad=grad, verbose=verbose, spin_orb=spin_orb)
+elseif opt_flavour == "frni" || opt_flavour == "full-rank-non-orthogonal"
+	num_classes = number_of_classes(frag_flavour)
+	class_train = rand(1:num_classes, α_max)
+	@show class_train
+	@time FRAGS = full_rank_non_iterative_driver(tbt, grad=grad, verbose=verbose, x0=x0, K0=class_train, spin_orb=spin_orb)
+else
+	error("Trying to do decomposition with optimization flavour $opt_flavour, not implemented!")
+end
+
+if POST == true
+	println("Starting post-processing...")
+	psi = get_wavefunction(h_ferm, wfs)
+
+	num_frags = length(FRAGS)
+
+	#LAST ENTRY HAS ONE-BODY TERM
+	EXPS = zeros(num_frags + 1)
+	VARS = zeros(num_frags + 1)
+	COEFFS = zeros(num_frags + 1)
+	h_meas = of.FermionOperator.zero()
+
+	println("Calculating expectation values and variances")
+	t00 = time()
+	for (i,frag) in enumerate(FRAGS)
+		op = fragment_to_ferm(frag)
+		global h_meas += op
+		EXPS[i] = expectation_value(op, psi)
+		VARS[i] = variance_value(op, psi)
+		COEFFS[i] = abs(frag.cn[1])
+	end
+	COEFFS[end] = 1
+	println("Finished after $(time() - t00) seconds...")
+
+	h_meas = Hccsd - h_meas
+	#@show of.normal_ordered(h_meas)
+
+	EXPS[end] = expectation_value(h_meas, psi)
+	VARS[end] = variance_value(h_meas, psi)
+
+	VARSUM = sum(sqrt.(VARS))
+	println("Fragment coefficients: ")
+	@show COEFFS
+	println("Full variances:")
+	@show VARS
+	println("Expectations of individual reflections (from 1 to -1), last one is one-body term")
+	@show EXPS .+ 1
+	println("Variance metric value is $(VARSUM^2)")
+
+
+	if PLOT == true
+		println("Starting plotting routine")
+		include("automatic_plotting.jl")
+	end
+
+end
---
+++ mp2_utils.py
@@ -0,0 +1,117 @@
+"""
+Functions related to obtaining MP2 initial amplitudes 
+"""
+import numpy as np
+from openfermion import FermionOperator, normal_ordered, hermitian_conjugated, bravyi_kitaev, get_fermion_operator
+
+def get_spin_orbital_energies(orbital_energies):
+    ''' 
+    Get the list of orbital energies. Return alpha/beta spin-orbital energies. 
+
+    Args:
+        orbital_energies: N orbital energies [..., e_i, ...]
+    
+    Returns:
+        spin_orbital_energies: 2N spin orbital energies. [..., e_i, e_i, ...]
+    '''
+    norb = len(orbital_energies)
+    spin_orbital_energies = np.zeros(2*norb)
+    for i in range(norb):
+        spin_orbital_energies[2*i] = orbital_energies[i]
+        spin_orbital_energies[2*i+1] = orbital_energies[i]
+    return spin_orbital_energies
+
+def get_full_two_body_integrals(mol):
+    '''
+    Given MolecularData with Hamiltonian H = one_body + 1/2 * h_{pqrs} a^+_p a^+_q a_r a_s 
+    Return h_{pqrs} in a 4-mode tensor 
+
+    Args:
+        mol: MolecularData of the Hamiltonian of interest
+    
+    Returns:
+        h_{pqrs}: The two-body coefficients (multiplied by 2). 
+    '''
+    H = get_fermion_operator(mol.get_molecular_hamiltonian())
+
+    n_spinorb = 2 * mol.n_orbitals
+    h_pqrs = np.zeros((n_spinorb, n_spinorb, n_spinorb, n_spinorb))
+    for term, val in H.terms.items():
+        if len(term) == 4:
+            p, q, r, s = term[0][0], term[1][0], term[2][0], term[3][0], 
+            h_pqrs[p, q, r, s] = 2*val
+    return h_pqrs
+
+def get_ccsd_from_mp2(orbital_energies, two_body_integrals, occ):
+    '''
+    Obtain the ccsd initial guesses from orbital energies & two-body integrals
+    '''
+    n_spinorb = len(orbital_energies)
+    orbs = list(np.arange(0, n_spinorb))
+    vir = [x for x in orbs if x not in occ] # Collect virtual orbitals 
+
+    ccsd_amp = np.zeros((n_spinorb, n_spinorb, n_spinorb, n_spinorb))
+    for j in range(len(occ)):
+        for i in range(j+1, len(occ)):
+            for b in range(len(vir)):
+                for a in range(b+1, len(vir)): 
+                    # Following the formula. See theory/1701.02691.png or the paper
+                    i_orb, j_orb, a_orb, b_orb = occ[i], occ[j], vir[a], vir[b]
+                    ccsd_amp[i_orb, j_orb, a_orb, b_orb] =  \
+                        two_body_integrals[i_orb, j_orb, b_orb, a_orb] - \
+                        two_body_integrals[i_orb, j_orb, a_orb, b_orb] 
+                    ccsd_amp[i_orb, j_orb, a_orb, b_orb] /= \
+                        orbital_energies[i_orb] + orbital_energies[j_orb] -\
+                        orbital_energies[a_orb] - orbital_energies[b_orb]
+    return -ccsd_amp
+
+def get_ccsd_amp(mol):
+    '''
+    Obtain MP2 CCSD amplitude from MolecularData
+    '''
+    occ = list(np.arange(0, mol.n_electrons))
+    spin_orbital_energies = get_spin_orbital_energies(mol.orbital_energies)
+    full_tbi = get_full_two_body_integrals(mol)
+    ccsd_amp = get_ccsd_from_mp2(spin_orbital_energies, full_tbi, occ)
+    ccsd_amp[abs(ccsd_amp) < 1e-10] = 0 # Zero low coefficient
+    return ccsd_amp
+
+def get_ccsd_op_from_amp(ccsd_amp):
+    '''
+    Obtain the ccsd operator from amplitudes 
+    '''
+    nzeros = np.nonzero(ccsd_amp) # in format ([1st indices], [2nd indices], ...)
+    nind = len(nzeros[0])
+
+    op = FermionOperator.zero()
+    for curind in range(nind): # Going over each orbital, produce operator for each spin pair
+        i, j, a, b = nzeros[0][curind], nzeros[1][curind], nzeros[2][curind], nzeros[3][curind]
+        term = (
+            (int(a), 1), (int(b), 1),
+            (int(i), 0), (int(j), 0)
+        )
+        op += FermionOperator(term=term, coefficient=ccsd_amp[i, j, a, b])
+    return op
+
+def get_ccsd_op(mol):
+    '''
+    Obtain the CCSD operator from MolecularData 
+    '''
+    ccsd_amp = get_ccsd_amp(mol)
+    ccsd_op = get_ccsd_op_from_amp(ccsd_amp)
+    return ccsd_op
+
+def get_qubit_uccsd(mol):
+    '''
+    The top level function. Accepts a MolecularData object and compute BK UCCSD operator 
+    based on MP2 amplitudes
+
+    Args:
+        mol: MolecularData of interesting Molecule
+    
+    Returns:
+        uccsd_bk: Bravyi-Kitaev form of UCCSD operator 
+    '''
+    ccsd_op = get_ccsd_op(mol)
+    uccsd_op = normal_ordered(ccsd_op + hermitian_conjugated(ccsd_op))
+    return bravyi_kitaev(uccsd_op, mol.n_qubits)
