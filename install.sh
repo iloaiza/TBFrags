2a27d7dff331fbc8ad5ff4f92d9c2c358b310c66
Ignacio Loaiza <ignacio.loaiza@mail.utoronto.ca>
2022-03-29 14:37:07 -0400

    Modified script so it also builds PyCall with correct python environment

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 # installs all necessary julia packages for running. Requires an existing installation of both julia and python
+# run script while python environment is active
 
 touch install.jl
 echo 'import Pkg
@@ -12,7 +13,10 @@ Pkg.add("Plots")
 Pkg.add("SparseArrays")
 Pkg.add("Arpack")
 Pkg.add("ExpmV")
-Pkg.add("PyPlot")' > install.jl
+Pkg.add("PyPlot")
+ENV["PYTHON"] = Sys.which("python")
+println("""Building PyCall with python executable $(ENV["PYTHON"])""")
+Pkg.build("PyCall")' > install.jl
 echo 'Starting julia packages installation...'
 julia install.jl
 echo 'Finished packages installation (unless julia package manager error message)'
