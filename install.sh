9d2c6641b0c1acf1a4e320593b186afbd2d5406b
Ignacio Loaiza <ignacio.loaiza@mail.utoronto.ca>
2022-03-29 14:56:22 -0400

    Added python installation routine

--- install.sh
+++ install.sh
@@ -1,5 +1,19 @@
+#!/bin/bash
+# creates and downloads python virtual environment with necessary packages if PY_INSTALL = "true", 0 skips to julia instalation
 # installs all necessary julia packages for running. Requires an existing installation of both julia and python
 # run script while python environment is active
+PY_INSTALL=true 
+
+if $PY_INSTALL
+then
+	echo 'Creating python environment and installing python packages...'
+	#loads module, for environments where necessary for running python...
+	module load python/3.7
+	#--system-site-packages option makes sure configuration from system is taken for packages (e.g. OpenBLAS)
+	virtualenv --system-site-packages venv
+	source venv/bin/activate
+	pip install openfermion pyscf openfermionpyscf tequila-basic
+fi
 
 touch install.jl
 echo 'import Pkg
