09766999d9ffa8610013b5377e82c99a17fd9b77
Ignacio Loaiza <ignacio.loaiza@mail.utoronto.ca>
2022-04-12 17:10:33 -0400

    Added readability

---
+++ ALL.sh
@@ -0,0 +1,4 @@
+../../JULIA/julia/julia -p 40 H_POST.jl h2 | tee TEXT/POST_H2.txt
+../../JULIA/julia/julia -p 40 H_POST.jl lih | tee TEXT/POST_LIH.txt
+../../JULIA/julia/julia -p 40 H_POST.jl beh2 | tee TEXT/POST_BEH2.txt
+../../JULIA/julia/julia -p 40 H_POST.jl h2o | tee TEXT/POST_H2O.txt
