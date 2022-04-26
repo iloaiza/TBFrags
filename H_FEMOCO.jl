bf26f8fd86e6df526bbbc2263be5da4bf1b961d9
Ignacio Loaiza <ignacio.loaiza@mail.utoronto.ca>
2022-04-26 14:52:25 -0400

    Removed unnecessary sanity check, added useful printouts

--- H_FEMOCO.jl
+++ H_FEMOCO.jl
@@ -16,17 +16,14 @@ println("Loading calculations with:")
 
 tbt_so, h_ferm = load_full_ham_tbt(ham_name, spin_orb=false, prefix=".")
 
-SVD_CARTAN_TBTS, SVD_TBTS = tbt_svd(tbt_so, tol=1e-6, spin_orb=true)
-
-α_SVD = size(SVD_TBTS)[1]
-println("SVD sanity (difference should be small):")
-svd_tbt = SVD_TBTS[1,:,:,:,:]
-for i in 2:α_SVD
-	svd_tbt += SVD_TBTS[i,:,:,:,:]
-end
-@show sum(abs.(tbt_so - svd_tbt))
+tol = 1e-8
+println("Starting SVD routine with cutoff tolerance $tol:")
+@time SVD_CARTAN_TBTS, SVD_TBTS = tbt_svd(tbt_so, tol=tol, spin_orb=true)
 
 n = size(tbt_so)[1]
 n_qubit = n
-H_full_q = qubit_transform(h_ferm)
+println("Performing fermion to qubit mapping:")
+@time H_full_q = qubit_transform(h_ferm)
+
+println("Qubit treatment of Hamiltonian:")
 qubit_treatment(H_full_q)
