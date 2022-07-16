1f48a40e45efe25ea966fc73ffa69772cdf132a3
Ignacio Loaiza <ignacio.loaiza@mail.utoronto.ca>
2022-07-16 19:12:40 -0400

    Prepared for running everything on Niagara

--- ALL.sh
+++ ALL.sh
@@ -1,4 +1,14 @@
-../../JULIA/julia/julia -p 40 H_POST.jl h2 | tee TEXT/POST_H2.txt
-../../JULIA/julia/julia -p 40 H_POST.jl lih | tee TEXT/POST_LIH.txt
-../../JULIA/julia/julia -p 40 H_POST.jl beh2 | tee TEXT/POST_BEH2.txt
-../../JULIA/julia/julia -p 40 H_POST.jl h2o | tee TEXT/POST_H2O.txt
+julia -p 40 H_FEMOCO.jl h2 | tee TEXT/L1_H2.txt
+julia -p 40 H_INTERACTION.jl h2 | tee TEXT/INTERACTION_H2.txt
+
+julia -p 40 H_FEMOCO.jl lih | tee TEXT/L1_LIH.txt
+julia -p 40 H_INTERACTION.jl lih | tee TEXT/INTERACTION_LIH.txt
+
+julia -p 40 H_FEMOCO.jl beh2 | tee TEXT/L1_BEH2.txt
+julia -p 40 H_INTERACTION.jl beh2 | tee TEXT/INTERACTION_BEH2.txt
+
+julia -p 40 H_FEMOCO.jl h2o | tee TEXT/L1_H2O.txt
+julia -p 40 H_INTERACTION.jl h2o | tee TEXT/INTERACTION_H2O.txt
+
+julia -p 40 H_FEMOCO.jl nh3 | tee TEXT/L1_NH3.txt
+julia -p 40 H_INTERACTION.jl nh3 | tee TEXT/INTERACTION_NH3.txt
